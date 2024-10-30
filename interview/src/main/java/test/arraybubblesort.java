8cec38757d4f32ec6c3cddaf0d0907a17ce9e8d7
Rijo johnson <rijojohnson@Rijos-MacBook-Pro.local>
2024-10-30 00:21:24 -0400

    bubble sort array

---
+++ interview/src/main/java/test/arraybubblesort.java
@@ -0,0 +1,26 @@
+package test;
+
+import java.util.Arrays;
+
+public class arraybubblesort {
+    public static void main(String[] args) {
+        
+        int a[] = {4,2,5,1,3};
+
+        System.out.println("Array before Sorting ..." + Arrays.toString(a));
+        
+        int n = a.length;
+        
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=0;j<n-1;j++){
+                if(a[j]>a[j+1]){
+                    int temp = a[j];
+                    a[j]= a[j+1];
+                    a[j+1]= temp;
+                }
+            }
+        }
+        System.out.println("Array after Sorting ..." + Arrays.toString(a));
+    }
+}
